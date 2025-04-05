b8192329a00913aa00f139f0b4f89992f97b0c8e
Zhaoyang Hong <297775278@qq.com>
2025-04-05 16:27:48 -0500

    To TPC-H

--- dss.h
+++ dss.h
@@ -506,7 +506,7 @@ extern tdef tdefs[];
 int dbg_print(int dt, FILE *tgt, void *data, int len, int eol);
 #define PR_STR(f, str, len)		dbg_print(DT_STR, f, (void *)str, len, 1)
 #define PR_VSTR(f, str, len) 	dbg_print(DT_VSTR, f, (void *)str, len, 1)
-#define PR_VSTR_LAST(f, str, len) 	dbg_print(DT_VSTR, f, (void *)str, len, 0)
+#define PR_VSTR_LAST(f, str, len) 	dbg_print(DT_VSTR, f, (void *)str, len, -1)
 #define PR_INT(f, str) 			dbg_print(DT_INT, f, (void *)str, 0, 1)
 #define PR_HUGE(f, str) 		dbg_print(DT_HUGE, f, (void *)str, 0, 1)
 #define PR_KEY(f, str) 			dbg_print(DT_KEY, f, (void *)str, 0, -1)
